76e6731555dc8874de6a6053457800a21ee4dfee
Catalina Vrinceanu <cnv98@yahoo.com>
2026-03-23 15:34:28 +0100

    book routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 import authRoutes from './routes/authRoutes';
 import libraryEntryRoutes from './routes/libraryEntryRoutes';
+import bookRoutes from './routes/bookRoutes';
 
 const router: Router = Router();
 
@@ -12,5 +13,6 @@ router.get('/', (req: Request, res: Response) => {
 // Mount auth routes
 router.use('/auth', authRoutes);
 router.use('/library', libraryEntryRoutes);
+router.use('/books', bookRoutes);
 
 export default router;
